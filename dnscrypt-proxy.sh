3f36b23215f157d47cc447717fc8155067d9f014
dr <z540453380@gmail.com>
2020-12-23 11:51:57 +0800

    Update dnscrypt-proxy.sh

--- dnscrypt-proxy.sh
+++ dnscrypt-proxy.sh
@@ -12,6 +12,7 @@ if [[ -n ${myipv6} ]]; then
     block_ipv6="false"
   fi
 fi
+rm -rf /etc/dnscrypt-proxy/dnscrypt-proxy.toml
     cat > '/etc/dnscrypt-proxy/dnscrypt-proxy.toml' << EOF
 #!!! Do not change these settings unless you know what you are doing !!!
 listen_addresses = ['127.0.0.1:53','[::1]:53']
@@ -31,7 +32,7 @@ keepalive = 30
 lb_estimator = true
 log_level = 2
 use_syslog = true
-#log_file = '/var/log/dnscrypt-proxy/dnscrypt-proxy.log'
+log_file = '/var/log/dnscrypt-proxy/dnscrypt-proxy.log'
 cert_refresh_delay = 86400
 tls_disable_session_tickets = false
 #tls_cipher_suite = [4865]
@@ -40,7 +41,7 @@ ignore_system_dns = true
 netprobe_timeout = 60
 netprobe_address = '1.1.1.1:53'
 # Maximum log files size in MB - Set to 0 for unlimited.
-log_files_max_size = 0
+log_files_max_size = 1024
 # How long to keep backup files, in days
 log_files_max_age = 7
 # Maximum log files backups to keep (or 0 to keep all backups)
@@ -67,7 +68,7 @@ cache_neg_max_ttl = 600
 #cert_file = "/etc/certs/${domain}_ecc/fullchain.cer"
 #cert_key_file = "/etc/certs/${domain}_ecc/${domain}.key"
 [query_log]
-  #file = '/var/log/dnscrypt-proxy/query.log'
+  file = '/var/log/dnscrypt-proxy/query.log'
   format = 'tsv'
 
 #[blacklist]
@@ -94,6 +95,7 @@ cache_neg_max_ttl = 600
   refresh_delay = 72
   prefix = ''
 EOF
+rm -rf /etc/systemd/system/dnscrypt-proxy.service
   cat > '/etc/systemd/system/dnscrypt-proxy.service' << EOF
 [Unit]
 Description=DNSCrypt client proxy
@@ -124,6 +126,7 @@ systemctl enable dnscrypt-proxy.service
 clear
 colorEcho ${INFO} "Install dnscrypt-proxy ing"
 if [[ $(systemctl is-active dnsmasq) == active ]]; then
+	systemctl stop dnsmasq
 	systemctl disable dnsmasq
 fi
 dnsver=$(curl -s "https://api.github.com/repos/DNSCrypt/dnscrypt-proxy/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
@@ -154,5 +157,6 @@ if [[ $dnsmasq_install -eq 1 ]]; then
 		rm /etc/resolv.conf
 		touch /etc/resolv.conf
 		echo "nameserver 127.0.0.1" > '/etc/resolv.conf'
+		echo "options edns0 single-request-reopen" > '/etc/resolv.conf'
 		systemctl start dnscrypt-proxy
 fi
