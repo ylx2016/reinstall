e122397f0534640769f02cc468d6e8ce4913419f
dr <z540453380@gmail.com>
2021-01-04 16:52:58 +0800

    Update CentOSReinstall_beta_oraclelinux8.sh

--- CentOSReinstall_beta_oraclelinux8.sh
+++ CentOSReinstall_beta_oraclelinux8.sh
@@ -42,7 +42,7 @@ INIT_OS(){
     dnf makecache timer
     #yum groupinstall core -y --exclude="aic94xx-firmware* alsa-* btrfs-progs* iprutils ivtv* iwl*firmware libertas* NetworkManager* plymouth* irqbalance postfix tuned polkit*"
     yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-    yum install -y grub2 dhcp-client openssh-server passwd wget kernel nano network-scripts htop
+    yum install -y grub2 dhcp-client openssh-server passwd wget kernel nano network-scripts NetworkManager htop
 
     sed -i '/^#PermitRootLogin\s/s/.*/&\nPermitRootLogin yes/' /etc/ssh/sshd_config
     sed -i 's/#MaxAuthTries 6/MaxAuthTries 3/' /etc/ssh/sshd_config
