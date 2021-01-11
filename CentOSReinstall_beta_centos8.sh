dcbe40bae8a3d9f903388dd979cae6359a643e43
dr <z540453380@gmail.com>
2021-01-11 14:21:29 +0800

    Add files via upload

---
+++ CentOSReinstall_beta_centos8.sh
@@ -0,0 +1,223 @@
+#!/bin/bash
+
+# Default Password: Pwd@CentOS , Change it after installation !
+
+#IMGURL='https://github.com/ylx2016/reinstall/releases/download/CentOS-7.9.2009-x86_64-docker/CentOS-7.9.2009-x86_64-docker.tar.xz'
+IMGURL='https://github.com/CentOS/sig-cloud-instance-images/raw/CentOS-8-x86_64/docker/centos-8-x86_64.tar.xz'
+BUSYBOX='https://busybox.net/downloads/binaries/1.31.0-defconfig-multiarch-musl/busybox-x86_64'
+ROOTDIR='/os'
+
+DOWNLOAD_IMG(){
+    if command -v wget >/dev/null 2>&1 ;then
+        mkdir $ROOTDIR
+        wget -O "$ROOTDIR/os.tar.xz" $IMGURL
+        wget -O "$ROOTDIR/busybox" $BUSYBOX
+        chmod +x "$ROOTDIR/busybox"
+    else
+        echo "ERROR: wget not found !"
+        exit
+    fi
+}
+
+DELALL(){
+    cp /etc/fstab $ROOTDIR
+	sysbios="0"
+	sysefi="0"
+	sysefifile=""
+	if [ -f "/boot/efi/EFI/centos/grub.cfg" ]; then
+		sysefi="1"
+		sysefifile="/boot/efi/EFI/centos/grub.cfg"
+	elif [ -f "/boot/efi/EFI/redhat/grub.cfg" ]; then
+		sysefi="1"
+		sysefifile="/boot/efi/EFI/redhat/grub.cfg"
+	else
+		sysbios="1"
+	fi
+    if command -v chattr >/dev/null 2>&1; then
+        find / -type f \( ! -path '/dev/*' -and ! -path '/proc/*' -and ! -path '/sys/*' -and ! -path "$ROOTDIR/*" \) \
+            -exec chattr -i {} + 2>/dev/null || true
+    fi
+    find / \( ! -path '/dev/*' -and ! -path '/proc/*' -and ! -path '/sys/*' -and ! -path "$ROOTDIR/*" \) -delete 2>/dev/null || true
+}
+
+EXTRACT_IMG(){
+    xzcat="$ROOTDIR/busybox xzcat"
+    tar="$ROOTDIR/busybox tar"
+    $xzcat "$ROOTDIR/os.tar.xz" | $tar -x -C /
+    mv -f $ROOTDIR/fstab /etc
+}
+
+INIT_OS(){
+    echo "nameserver 8.8.8.8" > /etc/resolv.conf
+    echo "nameserver 1.1.1.1" >> /etc/resolv.conf
+    echo "nameserver 9.9.9.9" >> /etc/resolv.conf
+    rm -f /root/anaconda-ks.cfg
+    export LC_ALL=en_US.UTF-8
+    yum makecache
+    yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+    yum install -y dhcp-client openssh-server passwd wget kernel kernel-core nano network-scripts htop
+    
+    device=$(fdisk -l | grep -o /dev/*da | head -1)
+	if [[ ${sysefi} == "1" ]];then
+		cd /
+		yum install grub2-efi grub2-efi-modules shim -y
+		grub2-install --target=x86_64-efi --bootloader-id=redhat --efi-directory=/boot/efi --verbose $device --boot-directory=/boot/efi
+		grub2-mkconfig -o /boot/efi/EFI/redhat/grub.cfg
+		grub2-install --target=x86_64-efi --bootloader-id=redhat --efi-directory=/boot/efi --verbose $device --boot-directory=/boot/efi
+	elif [[ ${sysbios} == "1" ]];then
+		yum install -y grub2
+		cd /
+		grub2-install $device
+		echo -e "GRUB_TIMEOUT=5\nGRUB_CMDLINE_LINUX=\"net.ifnames=0\"" > /etc/default/grub
+		grub2-mkconfig -o /boot/grub2/grub.cfg 2>/dev/null
+	fi	
+    sed -i '/^#PermitRootLogin\s/s/.*/&\nPermitRootLogin yes/' /etc/ssh/sshd_config
+    sed -i 's/#MaxAuthTries 6/MaxAuthTries 3/' /etc/ssh/sshd_config
+    sed -i 's/GSSAPIAuthentication yes/GSSAPIAuthentication no/' /etc/ssh/sshd_config
+    sed -i 's/#ClientAliveInterval 0/ClientAliveInterval 30/' /etc/ssh/sshd_config
+    sed -i 's/#UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
+    echo "net.core.default_qdisc=fq" >> /etc/sysctl.d/99-sysctl.conf
+	echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.d/99-sysctl.conf
+    systemctl enable sshd
+    systemctl enable network
+    echo "blog.ylx.me" | passwd --stdin root
+
+    touch /etc/sysconfig/network
+    cat >/etc/sysconfig/network-scripts/ifcfg-eth0 <<EOFILE
+    DEVICE=eth0
+    BOOTPROTO=static
+    ONBOOT=yes
+	IPADDR=$MAINIP
+	GATEWAY=$GATEWAYIP
+	NETMASK=$NETMASK
+	DNS1=1.1.1.1
+	DNS2=8.8.8.8
+EOFILE
+   
+    cat >>/etc/security/limits.conf<<EOFILE
+
+    * soft nofile 65535
+    * hard nofile 65535
+    * soft nproc 65535
+    * hard nproc 65535
+EOFILE
+    #sed -i 's/4096/65535/' /etc/security/limits.d/20-nproc.conf
+}
+
+function isValidIp() {
+  local ip=$1
+  local ret=1
+  if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+    ip=(${ip//\./ })
+    [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+    ret=$?
+  fi
+  return $ret
+}
+
+function ipCheck() {
+  isLegal=0
+  for add in $MAINIP $GATEWAYIP $NETMASK; do
+    isValidIp $add
+    if [ $? -eq 1 ]; then
+      isLegal=1
+    fi
+  done
+  return $isLegal
+}
+
+function GetIp() {
+  MAINIP=$(ip route get 1 | awk -F 'src ' '{print $2}' | awk '{print $1}')
+  GATEWAYIP=$(ip route | grep default | awk '{print $3}' | head -1)
+  SUBNET=$(ip -o -f inet addr show | awk '/scope global/{sub(/[^.]+\//,"0/",$4);print $4}' | head -1 | awk -F '/' '{print $2}')
+  value=$(( 0xffffffff ^ ((1 << (32 - $SUBNET)) - 1) ))
+  NETMASK="$(( (value >> 24) & 0xff )).$(( (value >> 16) & 0xff )).$(( (value >> 8) & 0xff )).$(( value & 0xff ))"
+}
+
+function UpdateIp() {
+  read -r -p "Your IP: " MAINIP
+  read -r -p "Your Gateway: " GATEWAYIP
+  read -r -p "Your Netmask: " NETMASK
+}
+
+function SetNetwork() {
+  isAuto='0'
+  if [[ -f '/etc/network/interfaces' ]];then
+    [[ ! -z "$(sed -n '/iface.*inet static/p' /etc/network/interfaces)" ]] && isAuto='1'
+    [[ -d /etc/network/interfaces.d ]] && {
+      cfgNum="$(find /etc/network/interfaces.d -name '*.cfg' |wc -l)" || cfgNum='0'
+      [[ "$cfgNum" -ne '0' ]] && {
+        for netConfig in `ls -1 /etc/network/interfaces.d/*.cfg`
+        do 
+          [[ ! -z "$(cat $netConfig | sed -n '/iface.*inet static/p')" ]] && isAuto='1'
+        done
+      }
+    }
+  fi
+  
+  if [[ -d '/etc/sysconfig/network-scripts' ]];then
+    cfgNum="$(find /etc/network/interfaces.d -name '*.cfg' |wc -l)" || cfgNum='0'
+    [[ "$cfgNum" -ne '0' ]] && {
+      for netConfig in `ls -1 /etc/sysconfig/network-scripts/ifcfg-* | grep -v 'lo$' | grep -v ':[0-9]\{1,\}'`
+      do 
+        [[ ! -z "$(cat $netConfig | sed -n '/BOOTPROTO.*[sS][tT][aA][tT][iI][cC]/p')" ]] && isAuto='1'
+      done
+    }
+  fi
+}
+
+function NetMode() {
+
+  # if [ "$isAuto" == '0' ]; then
+    # read -r -p "Using DHCP to configure network automatically? [Y/n]:" input
+    # case $input in
+      # [yY][eE][sS]|[yY]) NETSTR='' ;;
+      # [nN][oO]|[nN]) isAuto='1' ;;
+      # *) clear; echo "Canceled by user!"; exit 1;;
+    # esac
+  # fi
+  isAuto='1'
+
+  if [ "$isAuto" == '1' ]; then
+    GetIp
+    ipCheck
+    if [ $? -ne 0 ]; then
+      echo -e "Error occurred when detecting ip. Please input manually.\n"
+      UpdateIp
+    else
+      
+      echo "IP: $MAINIP"
+      echo "Gateway: $GATEWAYIP"
+      echo "Netmask: $NETMASK"
+      echo -e "\n"
+      read -r -p "Confirm? [Y/n]:" input
+      case $input in
+        [yY][eE][sS]|[yY]) ;;
+        [nN][oO]|[nN])
+          echo -e "\n"
+          UpdateIp
+          ipCheck
+          [[ $? -ne 0 ]] && {
+            clear
+            echo -e "Input error!\n"
+            exit 1
+          }
+        ;;
+        *) clear; echo "Canceled by user!"; exit 1;;
+      esac
+    fi
+    NETSTR="--ip-addr ${MAINIP} --ip-gate ${GATEWAYIP} --ip-mask ${NETMASK}"
+  fi
+}
+
+SetNetwork
+NetMode
+DOWNLOAD_IMG
+DELALL
+EXTRACT_IMG
+INIT_OS
+
+rm -rf $ROOTDIR
+yum clean all
+sync
+reboot -f
