cdaafefab4800844a5c94c7f97d9cb19ce28b905
dr <z540453380@gmail.com>
2021-01-11 11:45:32 +0800

    Add files via upload

--- CentOSReinstall_beta_CentOS7.sh
+++ CentOSReinstall_beta_CentOS7.sh
@@ -3,8 +3,8 @@
 # Default Password: Pwd@CentOS , Change it after installation !
 
 #IMGURL='https://github.com/CentOS/sig-cloud-instance-images/raw/CentOS-7.8.2003-x86_64/docker/centos-7.8.2003-x86_64-docker.tar.xz'
-IMGURL='https://github.com/ylx2016/reinstall/releases/download/CentOS-7.9.2009-x86_64-docker/CentOS-7.9.2009-x86_64-docker.tar.xz'
-BUSYBOX='https://github.com/ylx2016/reinstall/raw/master/busybox-x86_64'
+IMGURL='https://github.com/CentOS/sig-cloud-instance-images/raw/CentOS-7-x86_64/docker/centos-7-x86_64-docker.tar.xz'
+BUSYBOX='https://busybox.net/downloads/binaries/1.31.0-defconfig-multiarch-musl/busybox-x86_64'
 ROOTDIR='/os'
 
 DOWNLOAD_IMG(){
