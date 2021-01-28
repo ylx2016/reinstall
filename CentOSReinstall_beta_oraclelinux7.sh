1d6ab02d4998046eaf9141ddebb65b0ff0ddd793
dr <z540453380@gmail.com>
2021-01-28 16:36:45 +0800

    Update CentOSReinstall_beta_oraclelinux7.sh

--- CentOSReinstall_beta_oraclelinux7.sh
+++ CentOSReinstall_beta_oraclelinux7.sh
@@ -91,8 +91,8 @@ INIT_OS(){
 		grub2-mkconfig -o /boot/efi/EFI/redhat/grub.cfg
 		grub2-install --target=x86_64-efi --bootloader-id=redhat --efi-directory=/boot/efi --verbose $device --boot-directory=/boot/efi
 	elif [[ ${sysbios} == "1" ]];then
-		yum install -y https://github.com/ylx2016/kernel/releases/download/cloud/kernel-5.10.3_cloud-1.x86_64.rpm
-		yum install -y https://github.com/ylx2016/kernel/releases/download/cloud/kernel-headers-5.10.3_cloud-1.x86_64.rpm
+		#yum install -y https://github.com/ylx2016/kernel/releases/download/cloud/kernel-5.10.3_cloud-1.x86_64.rpm
+		#yum install -y https://github.com/ylx2016/kernel/releases/download/cloud/kernel-headers-5.10.3_cloud-1.x86_64.rpm
 		#yum install -y grub2
 		cd /
 		grub2-install $device
