dfa9f9a35dcdec0b37751d5e73bb1abcbdaf5627
dr <z540453380@gmail.com>
2021-03-02 15:29:54 +0800

    Update CentOSReinstall_beta_CentOS7.sh

--- CentOSReinstall_beta_CentOS7.sh
+++ CentOSReinstall_beta_CentOS7.sh
@@ -178,7 +178,7 @@ EOFILE
     echo "ylx2016" >> /etc/hostname
     echo "127.0.0.1 ylx2016" >> /etc/hosts
     sed -i 's/4096/65535/' /etc/security/limits.d/20-nproc.conf
-    wget -O /root/tcpx.sh "https://github.000060000.xyz/tcpx.sh" && /usr/bin/chmod +x /root/tcpx.sh
+    /usr/bin/wget -O /root/tcpx.sh "https://github.000060000.xyz/tcpx.sh" && /usr/bin/chmod +x /root/tcpx.sh
 }
 
 function isValidIp() {
