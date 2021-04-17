15c7761d2dcd3402841a546154c047821681276d
dr <z540453380@gmail.com>
2021-04-17 08:04:46 +0800

    Update UbuntuReinstall_beta_ubuntu18.sh

--- UbuntuReinstall_beta_ubuntu18.sh
+++ UbuntuReinstall_beta_ubuntu18.sh
@@ -153,6 +153,7 @@ iface $network_adapter_name inet static
 address $MAINIP
 netmask $NETMASK
 gateway $GATEWAYIP
+iface $network_adapter_name inet6 dhcp
 EOFILE
 	else
 cat >/etc/network/interfaces <<EOFILE
@@ -161,6 +162,7 @@ iface lo inet loopback
 
 auto $network_adapter_name
 iface $network_adapter_name inet dhcp
+iface $network_adapter_name inet6 dhcp
 EOFILE
 	fi
 
