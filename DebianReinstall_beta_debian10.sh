9e42798fd9b2c130ac0f49c103b2e8786cb91269
dr <z540453380@gmail.com>
2021-05-27 10:10:58 +0800

    Update DebianReinstall_beta_debian10.sh

--- DebianReinstall_beta_debian10.sh
+++ DebianReinstall_beta_debian10.sh
@@ -21,15 +21,20 @@ bit=`uname -m`
 
 # Default Password: blog.ylx.me , Change it after installation ! By dansnow and YLX
 
-urldata=$(rm -rf /tmp/url.tmp && curl -o /tmp/url.tmp 'https://cf-image.ylx.workers.dev/images/debian/buster/amd64/cloud/?C=M;O=D' && grep -o 2.......[\_]..[\:].. /tmp/url.tmp | head -n 1)
+
 if [[ ${bit} == "x86_64" ]]; then
+	urldata=$(rm -rf /tmp/url.tmp && curl -o /tmp/url.tmp 'https://cf-image.ylx.workers.dev/images/debian/buster/amd64/cloud/?C=M;O=D' && grep -o 2.......[\_]..[\:].. /tmp/url.tmp | head -n 1)
 	IMGURL=https://cf-image.ylx.workers.dev/images/debian/buster/amd64/cloud/${urldata}/rootfs.tar.xz
 	#IMGURL='https://github.com/debuerreotype/docker-debian-artifacts/raw/dist-amd64/buster/rootfs.tar.xz'
 	CN_IMGURL=https://mirrors.tuna.tsinghua.edu.cn/lxc-images/images/debian/buster/amd64/cloud/${urldata}/rootfs.tar.xz
 elif [[ ${bit} == "aarch64" ]]; then
+	urldata=$(rm -rf /tmp/url.tmp && curl -o /tmp/url.tmp 'https://cf-image.ylx.workers.dev/images/debian/buster/armhf/cloud/?C=M;O=D' && grep -o 2.......[\_]..[\:].. /tmp/url.tmp | head -n 1)
 	IMGURL=https://cf-image.ylx.workers.dev/images/debian/buster/armhf/cloud/${urldata}/rootfs.tar.xz
 	#IMGURL='https://github.com/debuerreotype/docker-debian-artifacts/raw/dist-amd64/buster/rootfs.tar.xz'
 	CN_IMGURL=https://mirrors.tuna.tsinghua.edu.cn/lxc-images/images/debian/buster/armhf/cloud/${urldata}/rootfs.tar.xz
+elif
+	echo "此系统骨骼太清奇，不支持！"
+	exit
 fi
 
 BUSYBOX='https://busybox.net/downloads/binaries/1.31.0-defconfig-multiarch-musl/busybox-x86_64'
