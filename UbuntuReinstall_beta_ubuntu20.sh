aaeed6a77e8a1b2325ebba67af1fff58240b518d
dr <z540453380@gmail.com>
2021-05-27 11:57:58 +0800

    Update UbuntuReinstall_beta_ubuntu20.sh

--- UbuntuReinstall_beta_ubuntu20.sh
+++ UbuntuReinstall_beta_ubuntu20.sh
@@ -97,6 +97,8 @@ DELALL(){
 		sysefi="1"
 	elif [ -f "/boot/efi/boot/grub/grub.cfg" ]; then
 		sysefi="1"
+	elif [ -f "/boot/efi/EFI/grub/grub.cfg" ]; then
+		sysefi="1"	
 	elif [ -f "/boot/efi/EFI/ubuntu/grub.cfg" ]; then
 		sysefi="1"
 	elif [ -f "/boot/efi/EFI/debian/grub.cfg" ]; then
@@ -133,6 +135,8 @@ INIT_OS(){
     rm -f /root/anaconda-ks.cfg
     export LC_ALL=C.UTF-8
     apt-get update
+	bit=`uname -m`
+	cd /
 	DEBIAN_FRONTEND=noninteractive apt-get install -y systemd openssh-server passwd wget nano linux-image-generic linux-headers-generic htop net-tools isc-dhcp-client ifplugd ifupdown ifmetric ifscheme ethtool guessnet fdisk coreutils curl sudo -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
 	DEBIAN_FRONTEND=noninteractive apt-get install -y grub2* -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
 	
