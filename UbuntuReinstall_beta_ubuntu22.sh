de4d83b93e1584fb1993706c02dca62a4ecd98c2
dr <z540453380@gmail.com>
2024-03-21 10:30:49 +0800

    Update UbuntuReinstall_beta_ubuntu22.sh

--- UbuntuReinstall_beta_ubuntu22.sh
+++ UbuntuReinstall_beta_ubuntu22.sh
@@ -1,41 +1,12 @@
 #!/usr/bin/env bash
+# 设置环境变量，确保脚本能正确使用系统命令
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-# Default Password: blog.ylx.me , Change it after installation ! By dansnow and YLX
+# 提醒用户更改默认密码。此处可以加强，强调安全性重要性
+echo "注意：默认密码为 blog.ylx.me，请在安装后立即更改！"
 
-_exists() {
-  local cmd="$1"
-  if eval type type >/dev/null 2>&1; then
-    eval type "$cmd" >/dev/null 2>&1
-  elif command >/dev/null 2>&1; then
-    command -v "$cmd" >/dev/null 2>&1
-  else
-    which "$cmd" >/dev/null 2>&1
-  fi
-  local rt=$?
-  return ${rt}
-}
-
-err() {
-  printf "\nError: %s.\n" "$1" 1>&2
-  exit 1
-}
-
-download() {
-
-    if _exists wget; then
-        wget -O "$2" "$1"
-    elif _exists curl; then
-        curl -fL "$1" -o "$2"
-    elif _exists busybox && busybox wget --help > /dev/null 2>&1; then
-        busybox wget -O "$2" "$1"
-    else
-        err 'Cannot find "wget", "curl" or "busybox wget" to download files'
-    fi
-}
-
-#from bohanyang/debi
+#from bohanyang/debi 处理传进来的参数
 while [ $# -gt 0 ]; do
   case $1 in
 
@@ -50,7 +21,11 @@ while [ $# -gt 0 ]; do
   --ssh-port)
     ssh_port=$2
     shift
-    ;;	
+    ;;
+  --hostname)
+    tmpHostName=$2
+    shift
+    ;;
   *)
     err "Unknown option: \"$1\""
     ;;
@@ -58,28 +33,62 @@ while [ $# -gt 0 ]; do
   shift
 done
 
+# 检查bash是否在/usr/bin/bash，如果不在，则创建链接
 if [ ! -f "/usr/bin/bash" ]; then
   ln $(which bash) /usr/bin/bash
 fi
 
+# 检查并安装curl
 if ! type curl >/dev/null 2>&1; then
-  echo 'curl 未安装 安装中'
+  echo 'curl 未安装，正在安装...'
   apt-get update && apt-get install curl -y || yum install curl -y
 else
   echo 'curl 已安装，继续'
 fi
 
+# 检查并安装wget
 if ! type wget >/dev/null 2>&1; then
-  echo 'wget 未安装 安装中'
+  echo 'wget 未安装，正在安装...'
   apt-get update && apt-get install wget -y || yum install curl -y
 else
   echo 'wget 已安装，继续'
 fi
 
-if [ ! -f "/usr/bin/wget" ]; then
-  ln $(which wget) /usr/bin/wget
+# 检查并安装 zip 和 tar
+if ! type zip >/dev/null 2>&1; then
+  echo 'zip 未安装，正在安装...'
+  if command -v apt-get >/dev/null 2>&1; then
+    sudo apt-get update && sudo apt-get install zip -y
+  elif command -v yum >/dev/null 2>&1; then
+    sudo yum install zip -y
+  else
+    echo '未知的包管理器，请手动安装 zip。'
+    exit 1
+  fi
+else
+  echo 'zip 已安装，继续'
 fi
 
+if ! type tar >/dev/null 2>&1; then
+  echo 'tar 未安装，正在安装...'
+  if command -v apt-get >/dev/null 2>&1; then
+    sudo apt-get update && sudo apt-get install tar -y
+  elif command -v yum >/dev/null 2>&1; then
+    sudo yum install tar -y
+  else
+    echo '未知的包管理器，请手动安装 tar。'
+    exit 1
+  fi
+else
+  echo 'tar 已安装，继续'
+fi
+
+# 定义变量以便于后续使用
+my_wget=$(which wget)
+my_curl=$(which curl)
+my_mkdir=$(which mkdir)
+
+# 根据系统架构下载不同的Debian镜像和BusyBox
 bit=$(uname -m)
 if [[ ${bit} == "x86_64" ]]; then
   urldata=$(rm -rf /tmp/url.tmp && curl -o /tmp/url.tmp 'https://cf-image.ylx.workers.dev/images/ubuntu/jammy/amd64/cloud/?C=M;O=D' && grep -o 2.......[\_]..[\:].. /tmp/url.tmp | head -n 1)
@@ -106,45 +115,35 @@ else
   echo "此系统骨骼太清奇，不支持！"
   exit
 fi
+
+# 设置下载根目录
 ROOTDIR='/os'
 
+# 下载系统镜像的函数
 DOWNLOAD_IMG() {
   if command -v wget >/dev/null 2>&1; then
-    mkdir $ROOTDIR
+    mkdir -p $ROOTDIR
     if [[ "$isCN" == '1' ]]; then
-      IMGURLstate=$(curl -s --head $CN_IMGURL | head -n 1)
-      if [[ ${IMGURLstate} == *200* ]]; then
-        echo "CN 镜像地址检查OK，继续！"
-      else
-        echo "CN 镜像地址检查出错，退出！"
-        exit 1
-      fi
-      BUSYBOXstate=$(curl -s --head $CN_BUSYBOX | head -n 1)
-      if [[ ${BUSYBOXstate} == *200* || ${BUSYBOXstate} == *308* ]]; then
-        echo "CN BUSYBOX镜像地址检查OK，继续！"
-      else
-        echo "CN BUSYBOX地址检查出错，退出！"
-        exit 1
-      fi
-      wget -O "$ROOTDIR/os.tar.xz" $CN_IMGURL
-      wget -O "$ROOTDIR/busybox" $CN_BUSYBOX
+      IMGURLstate=$(curl -k -s --head $CN_IMGURL | head -n 1)
+      BUSYBOXstate=$(curl -k -s --head $CN_BUSYBOX | head -n 1)
     else
-      IMGURLstate=$(curl -s --head $IMGURL | head -n 1)
-      if [[ ${IMGURLstate} == *200* ]]; then
-        echo "镜像地址检查OK，继续！"
-      else
-        echo "镜像地址检查出错，退出！"
-        exit 1
-      fi
-      BUSYBOXstate=$(curl -s --head $BUSYBOX | head -n 1)
-      if [[ ${BUSYBOXstate} == *200* ]]; then
-        echo "BUSYBOX地址检查OK，继续！"
-      else
-        echo "BUSYBOX地址检查出错，退出！"
-        exit 1
-      fi
-      wget -O "$ROOTDIR/os.tar.xz" $IMGURL
-      wget -O "$ROOTDIR/busybox" $BUSYBOX
+      IMGURLstate=$(curl -k -s --head $IMGURL | head -n 1)
+      BUSYBOXstate=$(curl -k -s --head $BUSYBOX | head -n 1)
+    fi
+    if [[ ${IMGURLstate} != *200* ]]; then
+      echo "镜像地址检查出错，退出！"
+      exit 1
+    fi
+    if [[ ${BUSYBOXstate} != *200* && ${BUSYBOXstate} != *308* ]]; then
+      echo "BUSYBOX地址检查出错，退出！"
+      exit 1
+    fi
+    if [[ "$isCN" == '1' ]]; then
+      wget --no-check-certificate -O "$ROOTDIR/os.tar.xz" $CN_IMGURL
+      wget --no-check-certificate -O "$ROOTDIR/busybox" $CN_BUSYBOX
+    else
+      wget --no-check-certificate -O "$ROOTDIR/os.tar.xz" $IMGURL
+      wget --no-check-certificate -O "$ROOTDIR/busybox" $BUSYBOX
     fi
     chmod +x "$ROOTDIR/busybox"
   else
@@ -153,11 +152,12 @@ DOWNLOAD_IMG() {
   fi
 }
 
+# 删除所有旧系统文件的函数
 DELALL() {
   cp /etc/fstab $ROOTDIR
   sysbios="0"
   sysefi="0"
-  #sysefifile=""
+  sysefifile=""
   if [ -d "/sys/firmware/efi" ]; then
     sysefi="1"
     # elif [ -f "/boot/efi/boot/grub/grub.cfg" ]; then
@@ -179,6 +179,7 @@ DELALL() {
   find / \( ! -path '/dev/*' -and ! -path '/proc/*' -and ! -path '/sys/*' -and ! -path "$ROOTDIR/*" \) -delete 2>/dev/null || true
 }
 
+# 解压新系统镜像的函数
 EXTRACT_IMG() {
   xzcat="$ROOTDIR/busybox xzcat"
   tar="$ROOTDIR/busybox tar"
@@ -186,13 +187,15 @@ EXTRACT_IMG() {
   mv -f $ROOTDIR/fstab /etc
 }
 
+# 初始化新系统的函数
 INIT_OS() {
+  cd /
   rm -rf /etc/resolv.conf
   touch /etc/resolv.conf
   if [[ "$isCN" == '1' ]]; then
     echo "nameserver 114.114.114.114" >/etc/resolv.conf
     echo "nameserver 223.5.5.5" >>/etc/resolv.conf
-    sed -i 's#http://archive.ubuntu.com#http://mirrors.163.com#g' /etc/apt/sources.list
+    sed -i 's#http://deb.debian.org#http://mirrors.163.com#g' /etc/apt/sources.list
   else
     echo "nameserver 1.1.1.1" >/etc/resolv.conf
     echo "nameserver 8.8.8.8" >>/etc/resolv.conf
@@ -207,55 +210,63 @@ INIT_OS() {
   DEBIAN_FRONTEND=noninteractive apt-get install -y grub2* -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
 
   device=$(fdisk -l | grep -o /dev/*da | head -1)
+  # 根据系统架构和启动方式安装GRUB
   if [[ ${sysefi} == "1" ]]; then
-    cd /
-    bit=$(uname -m)
+    echo "安装EFI模式的GRUB（架构：${bit}）"
+    # 针对不同架构选择合适的GRUB EFI包
     if [[ ${bit} == "x86_64" ]]; then
-      apt-get install -y grub-efi grub-efi-amd64
+      apt-get install -y grub-efi-amd64
     elif [[ ${bit} == "aarch64" ]]; then
-      #apt-get install -y efibootmgr grub-common grub2-common os-prober pv-grub-menu grub-uboot
-      #apt-get -y install grub2-common efivar grub-efi-arm64 os-prober pv-grub-menu grub-uboot efibootmgr
-      apt-get -y install grub2-common efivar grub-efi-arm64 efibootmgr
+      apt-get install -y grub2-common efivar grub-efi-arm64 efibootmgr
+    else
+      echo "不支持的架构：${bit}"
+      exit 1
     fi
-    grub-install
-    $(which update-grub)
+
+    # 安装GRUB到EFI分区并更新配置
+    grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=debian --recheck
+    update-grub
+
+    # 特定于x86_64架构的后续操作
     if [[ ${bit} == "x86_64" ]]; then
-      cd /boot/efi/EFI && mkdir boot && cp ubuntu/grubx64.efi boot/bootx64.efi
+      mkdir -p /boot/efi/EFI/boot
+      cp /boot/efi/EFI/debian/grubx64.efi /boot/efi/EFI/boot/bootx64.efi
     fi
-    cd /
   elif [[ ${sysbios} == "1" ]]; then
-    cd /
-    grub-install $device
-    $(which update-grub)
-    grub-install $device
+    echo "安装BIOS模式的GRUB（设备：${device}）"
+    apt-get install -y grub-pc
+    grub-install --target=i386-pc ${device}
+    update-grub
+  else
+    echo "无法确定系统的启动模式（EFI或BIOS）。"
+    exit 1
   fi
 
-  #sed -i '/Port /d' /etc/ssh/sshd_config
+  sed -i '/Port /d' /etc/ssh/sshd_config
   #echo "Port 52890" >>/etc/ssh/sshd_config
-  sed -i '/^#PermitRootLogin\s/s/.*/PermitRootLogin yes/' /etc/ssh/sshd_config
-  sed -i '/MaxAuthTries\s/s/.*/MaxAuthTries 3/' /etc/ssh/sshd_config
-  sed -i '/GSSAPIAuthentication\s/s/.*/GSSAPIAuthentication no/' /etc/ssh/sshd_config
-  sed -i '/ClientAliveInterval\s/s/.*/ClientAliveInterval 30/' /etc/ssh/sshd_config
-  sed -i '/UseDNS\s/s/.*/UseDNS no/' /etc/ssh/sshd_config
+  sed -i '/^#PermitRootLogin\s/s/.*/&\nPermitRootLogin yes/' /etc/ssh/sshd_config
+  sed -i 's/#MaxAuthTries 6/MaxAuthTries 3/' /etc/ssh/sshd_config
+  sed -i 's/GSSAPIAuthentication yes/GSSAPIAuthentication no/' /etc/ssh/sshd_config
+  sed -i 's/#ClientAliveInterval 0/ClientAliveInterval 30/' /etc/ssh/sshd_config
+  sed -i 's/#UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
   systemctl enable ssh
 
   echo -e "blog.ylx.me\nblog.ylx.me" | passwd "root"
+
   [ -n "$password" ] && echo -e "$password\n$password" | passwd "root"
 
   [ -n "$authorized_keys_url" ] && ! download "$authorized_keys_url" /dev/null &&
     err "Failed to download SSH authorized public keys from \"$authorized_keys_url\""
 
   [ -n "$authorized_keys_url" ] && mkdir -m 0700 -p /root/.ssh && wget -O /root/.ssh/authorized_keys $authorized_keys_url && sed -i '/PasswordAuthentication\s/s/.*/PasswordAuthentication no/' /etc/ssh/sshd_config
-  
+
   [ -n "$ssh_port" ] && sed -i "/Port\s/s/.*/Port ${ssh_port}/" /etc/ssh/sshd_config
-  
-  echo "net.core.default_qdisc=fq" >>/etc/sysctl.d/99-sysctl.conf
+
+  echo "net.core.default_qdisc=fq_pie" >>/etc/sysctl.d/99-sysctl.conf
   echo "net.ipv4.tcp_congestion_control=bbr" >>/etc/sysctl.d/99-sysctl.conf
 
   sed -i '/GRUB_CMDLINE_LINUX=/d' /etc/default/grub
   echo "GRUB_CMDLINE_LINUX=\"net.ifnames=0 biosdevname=0\"" >>/etc/default/grub
-  sed -i '/GRUB_TIMEOUT_STYLE=/d' /etc/default/grub
-  sed -i 's/^\(GRUB_TIMEOUT=\)[0-9]\+/\110/' /etc/default/grub
   $(which update-grub)
 
   systemctl enable networking
@@ -267,8 +278,7 @@ INIT_OS() {
    auto lo
 iface lo inet loopback
 
-#auto $network_adapter_name
-allow-hotplug $network_adapter_name
+auto $network_adapter_name
 iface $network_adapter_name inet static
 address $MAINIP
 netmask $NETMASK
@@ -279,8 +289,7 @@ EOFILE
    auto lo
 iface lo inet loopback
 
-#auto $network_adapter_name
-allow-hotplug $network_adapter_name
+auto $network_adapter_name
 iface $network_adapter_name inet dhcp
 iface $network_adapter_name inet6 dhcp
 EOFILE
@@ -295,8 +304,7 @@ EOFILE
 EOFILE
 
   $(which mkdir) -p /etc/systemd/system/networking.service.d/
-  echo -e "[Service]\nTimeoutStartSec=10sec" >/etc/systemd/system/networking.service.d/timeout.conf
-  echo -e "[Service]\nTimeoutStartSec=10sec" >/etc/systemd/system/networking.service.d/override.conf
+  echo -e "[Service]\nTimeoutStartSec=15sec" >/etc/systemd/system/networking.service.d/timeout.conf
 
   # sed -i 's/4096/65535/' /etc/security/limits.d/20-nproc.conf
   if [[ "$isCN" == '1' ]]; then
@@ -310,15 +318,19 @@ EOFILE
   echo "precedence ::ffff:0:0/96 100" >>/etc/gai.conf
   rm -rf /etc/hostname
   touch /etc/hostname
-  echo "ylx2016" >>/etc/hostname
-  echo "127.0.0.1 ylx2016" >>/etc/hosts
+
+  [[ -n "$tmpHostName" ]] && HostName="$tmpHostName" || HostName=$(hostname)
+  [[ -z "$HostName" || "$HostName" =~ "localhost" || "$HostName" =~ "localdomain" || "$HostName" == "random" ]] && HostName="instance-$(date "+%Y%m%d")-$(date "+%H%M")"
+
+  echo "$HostName" >>/etc/hostname
+  echo "127.0.0.1 $HostName" >>/etc/hosts
   $(which wget) -O /root/tcpx.sh "https://github.000060000.xyz/tcpx.sh" && $(which chmod) +x /root/tcpx.sh
   ln -fs /usr/bin/bash /usr/bin/sh
   timedatectl set-timezone Asia/Shanghai
-  systemctl disable unattended-upgrades networkd-dispatcher
 
 }
 
+# 检查IP合法性的函数
 function isValidIp() {
   local ip=$1
   local ret=1
@@ -330,6 +342,7 @@ function isValidIp() {
   return $ret
 }
 
+# 检查IP信息的函数
 function ipCheck() {
   isLegal=0
   for add in $MAINIP $GATEWAYIP $NETMASK; do
@@ -341,6 +354,7 @@ function ipCheck() {
   return $isLegal
 }
 
+# 获取IP信息的函数
 function GetIp() {
   MAINIP=$(ip route get 1 | awk -F 'src ' '{print $2}' | awk '{print $1}')
   GATEWAYIP=$(ip route | grep default | awk '{print $3}' | head -1)
@@ -349,12 +363,14 @@ function GetIp() {
   NETMASK="$(((value >> 24) & 0xff)).$(((value >> 16) & 0xff)).$(((value >> 8) & 0xff)).$((value & 0xff))"
 }
 
+# 更新IP信息的函数
 function UpdateIp() {
   read -r -p "Your IP: " MAINIP
   read -r -p "Your Gateway: " GATEWAYIP
   read -r -p "Your Netmask: " NETMASK
 }
 
+# 设置网络的函数
 function SetNetwork() {
   isCN='0'
   geoip=$(wget --no-check-certificate -qO- https://api.ip.sb/geoip -T 10 | grep "\"country_code\":\"CN\"")
@@ -385,6 +401,7 @@ function SetNetwork() {
   fi
 }
 
+# 网络模式设置的函数
 function NetMode() {
 
   if [ "$isAuto" == '0' ]; then
@@ -439,25 +456,25 @@ function NetMode() {
   fi
 }
 
+# 执行网络设置、下载、删除、解压和初始化操作
+
 SetNetwork
 NetMode
 
 DOWNLOAD_IMG
 DELALL
 EXTRACT_IMG
-sleep 5s
 INIT_OS
 
+# 清理安装后的临时文件并提示重启
 rm -rf $ROOTDIR
 apt-get clean all
 sync
-# reboot -f
+echo "安装完成，建议重启系统。"
 
 read -p "确认上面没有严重的错误信息，是否现在重启 ? [Y/n] :" yn
 [ -z "${yn}" ] && yn="y"
 if [[ $yn == [Yy] ]]; then
-  echo -e "VPS 重启中..."
-  sleep 10s
-  reboot -nf
-  echo 1 > /proc/sys/kernel/sysrq echo b > /proc/sysrq-trigger
+  echo -e "${Info} VPS 重启中..."
+  reboot -f
 fi
